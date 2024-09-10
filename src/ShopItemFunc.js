ceffbbf5852f69f2387c4b0eec535fb3b20add64
Yuliya Kalinina <u.v.zaika@gmail.com>
2024-09-10 10:59:42 +0400

    task done

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,28 @@
+export const ShopItemFunc = ({
+  brand,
+  title,
+  description,
+  descriptionFull,
+  price,
+  currency,
+}) => {
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">
+          {currency}
+          {price}.00
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+};
